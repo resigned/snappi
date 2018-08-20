e4833019908b7969e53ea034152bd21920a975b0
Jay <11010206+resigned@users.noreply.github.com>
2018-08-20 18:36:59 -0400

    for loops!

--- aya.js
+++ aya.js
@@ -49,58 +49,74 @@ class dank {
 
   route(method, route, ...func) {
     try {
-      this.routes[method.toUpperCase()].push([route.split("/"), func]);
+      this.routes[method.toUpperCase()].push([
+        route.split("/").filter(String),
+        func,
+      ]);
     } catch (err) {
       throw new Error(`Invalid route setup`);
     }
   }
 
   async h(req, res) {
-    try {
-      for (let func of this.pre_middleware) await func(req, res);
-    } catch (err) {
-      res.end(err.message);
-      return;
+    for (let i = 0; i < this.pre_middleware.length; i++) {
+      let next = await this.pre_middleware[i](req, res);
+      if (next != undefined) return;
     }
 
     let path = req.url.split("/");
-
-    try {
-      let r;
-      let routesReq = this.routes[req.method];
-      let routesLen = routesReq.length;
-      let outerI = 0;
-      for (;outerI<routesLen;outerI++) {
-        let route = routesReq[outerI];
-        let nRoute = route[0];
-        if (path.length != nRoute.length) continue;
-        let params = {};
-        let i = 0;
-        for (let tempRoute = nRoute.length; i < tempRoute; i++) {
-          let routePointer = nRoute[i];
-          if (routePointer.charAt() == ":") {
-            params[routePointer.substring(1)] = path[i];
-          } else if (path[i] != routePointer) break;
-        }
-        if (i != nRoute.length) continue;
-        req.params = params;
-        r = route;
-        break;
+    let tmp_arr = [];
+    for (let i = 0; i < path.length; i++) {
+      if (path[i] != "") {
+        tmp_arr.push(path[i]);
       }
+    }
+    path = tmp_arr;
 
-      if (r == undefined) throw new Error("Invalid route");
-      for (let func of [...this.aft_middleware, ...r[1]])
-        if (func instanceof Function) {
-          await func(req, res);
-        } else {
-          req = { ...req, ...func };
-        }
-    } catch (err) {
-      res.end(err.message);
+    let r;
+    let routesReq = this.routes[req.method];
+    let routesLen = routesReq.length;
+    let outerI = 0;
+    for (; outerI < routesLen; outerI++) {
+      let route = routesReq[outerI];
+      let nRoute = route[0];
+      if (path.length != nRoute.length) continue;
+      let params = {};
+      let i = 0;
+      for (let tempRoute = nRoute.length; i < tempRoute; i++) {
+        let routePointer = nRoute[i];
+        if (routePointer.charAt() == ":" && path[i].length != 0) {
+          params[routePointer.substring(1)] = path[i];
+        } else if (path[i] != routePointer) break;
+      }
+      if (i != nRoute.length) continue;
+      req.params = params;
+      r = route;
+      break;
+    }
+
+    if (r == undefined) {
+      res.end("Invalid route");
+      return;
+    }
+    for (let i = 0; i < r[1].length; i++) {
+      let func = r[1][i];
+      if (func instanceof Function) {
+        let next = await func(req, res);
+        if (next != undefined) return;
+      } else {
+        req = { ...req, ...func };
+      }
     }
   }
 
   start() {
+    let arr = Object.entries(this.routes);
+    for (let method of arr) {
+      for (let route of method[1]) {
+        route[1] = [...this.aft_middleware, ...route[1]];
+      }
+    }
     this.server = http
       .createServer(async (req, res) => {
         this.h(req, res);
